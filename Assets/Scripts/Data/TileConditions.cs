d538c976c1169d756aca03079f5826b8b5197a2e
NotPrepared <l.latussek@web.de>
2020-08-31 23:10:19 +0200

    Added options for using falloff map and height affecting temperature
    Also optimized the code a bit and added comments for clarity

--- Assets/Scripts/Data/TileConditions.cs
+++ Assets/Scripts/Data/TileConditions.cs
@@ -14,11 +14,10 @@ public class TileConditions : ScriptableObject {
 	public GameObject prefabTile;
 	
 	public bool CheckIfValid(float height, float humidity, float temperature)
-	//, float humidity, float temperature
 	{
-		Debug.Log("Height Value in Tile Condition:" + height.ToString());
+		/*Debug.Log("Height Value in Tile Condition:" + height.ToString());
 		Debug.Log("Humidity Value in Tile Condition:" + humidity.ToString());
-		Debug.Log("Temperature Value in Tile Condition:" + temperature.ToString());
+		Debug.Log("Temperature Value in Tile Condition:" + temperature.ToString());*/
 
 		bool validHeight = height > minHeight && height <= maxHeight;
 		bool validHumidity = (humidity > minHumidity) && (humidity <= maxHumidity);
--- Assets/Scripts/HexMapGenerator.cs
+++ Assets/Scripts/HexMapGenerator.cs
@@ -8,69 +8,106 @@ using UnityEngine.AI;
 
 public class HexMapGenerator : MonoBehaviour
 {
+    //Tilesizes
+    private static float tilesPer1UnitY = 4.5312f;
+    private static float tilesPer1UnitX = 0.75f;
+
+    private static float tilesPer1UnitZ = 0.865f;
+
+    //Scale settings
     public int length;
     public int width;
     public int scale;
     public float yScale;
+
+    //Map Settings
     public MapSettings heightMapSettings;
     public MapSettings humidityMapSettings;
     public MapSettings temperatureMapSettings;
     public GameObject waterPlane;
     public float waterHeight;
 
+    public bool useFalloff;
+    public bool heightAffectsTemperature;
+
+    //Tiles
     public TileConditions[] tileConditions;
 
+    //Navmesh
     public NavMeshSurface surface;
 
-    private static float tilesPer1UnitY = 4.5312f;
-    private static float tilesPer1UnitX = 0.75f;
-    private static float tilesPer1UnitZ = 0.865f;
     void Start()
     {
         GenerateTileMap();
         
-        surface.BuildNavMesh();
+        //surface.BuildNavMesh();
     }
 
     private void GenerateTileMap()
     {
-        Map heightMap = GenerateMap(length, width, heightMapSettings, Vector2.zero);
-        Map humidityMap = GenerateMap(length, width, humidityMapSettings, Vector2.zero);
-        Map temperatureMap = GenerateMap(length, width, temperatureMapSettings, Vector2.zero);
+        //Maps
+        Map falloffMap = GenerateFalloffMap(length);
+        Map heightMap = null;
+        Map humidityMap = null;
+        Map temperatureMap = null;
+        
+        //Create Height Map
+        if (useFalloff)
+        {
+            heightMap = GenerateMapWithMulitplier(length, width, heightMapSettings, Vector2.zero, falloffMap, false); 
+        }
+        else
+        {
+            heightMap = GenerateMap(length, width, heightMapSettings, Vector2.zero);
+        }
 
-        float[,] falloffMap = GenerateFalloffMap(length);
+        //Create Humidity Map
+        humidityMap = GenerateMap(length, width, humidityMapSettings, Vector2.zero);
+
+        //Create Temperature Map
+        if (heightAffectsTemperature)
+        {
+            temperatureMap = GenerateMapWithMulitplier(length, width, temperatureMapSettings, Vector2.zero, heightMap, true);
+        }
+        else
+        {
+            temperatureMap = GenerateMap(length, width, temperatureMapSettings, Vector2.zero);
+        }
+        
+        //Variables for scaling
         float zScale = tilesPer1UnitZ * scale;
         float xScale = tilesPer1UnitX * scale;
         yScale *= scale;
         float maxYDifference = yScale * tilesPer1UnitY;
-        Debug.Log("MaxYDif: " + maxYDifference.ToString());
-        
+
+        //Positioning Water plane
         waterPlane.transform.position= new Vector3(tilesPer1UnitX *scale*length/2f, waterHeight* yScale, tilesPer1UnitZ *scale*width/2f);
         waterPlane.transform.localScale= new Vector3(tilesPer1UnitX *length/2f, 1, tilesPer1UnitZ *width/2f);
         
-        
+        //Variables for max Map values
         float maxHeight = heightMap.maxValue;
         float maxTemperature = temperatureMap.maxValue;
         float maxHumidity = humidityMap.maxValue;
+        
+        //Generate Tiles by looping over x and z
         for (int xCoord = 0; xCoord < length; xCoord++)
         {
             float zOffset = (xCoord % 2) / 2.0f;
             float xCoordNormalized = xCoord * xScale;
             float zCoordNormalized;
+            float yCoordNormalized;
             for (int zCoord = 0; zCoord < width; zCoord++)
             {
+                //Get map data at position
                 zCoordNormalized = (zCoord + zOffset) * zScale;
-                float height = heightMap.values[xCoord, zCoord]  * falloffMap[xCoord, zCoord];
+                float height = heightMap.values[xCoord, zCoord];
                 float humidity = humidityMap.values[xCoord, zCoord];
                 float temperature = temperatureMap.values[xCoord, zCoord];
-                /*Debug.Log("Height Map value: " + (heightMap.values[xCoord, zCoord] / maxHeight).ToString());
-                Debug.Log("Falloff Map Value: " + falloffMap[xCoord, zCoord].ToString());
-                Debug.Log("Height Value: " + height.ToString());*/
-                
-                float yCoordNormalized = yScale * height;
-                //(maxYDifference/2.0f)
+              
+                yCoordNormalized = yScale * height;
                 Vector3 coordsForTile = new Vector3(xCoordNormalized, yCoordNormalized-maxYDifference/20, zCoordNormalized);
-
+                
+                //Find valid Tile and instantiate it
                 GameObject rightPrefab = findValidTile(height, humidity, temperature);
 
                 GameObject newTile = Instantiate(rightPrefab, coordsForTile, Quaternion.identity);
@@ -86,7 +123,8 @@ public class HexMapGenerator : MonoBehaviour
         {
             if (tileConditions[i].CheckIfValid(height, humidity, temperature)) return tileConditions[i].prefabTile;
         }
-
+        
+        //Should never happen 
         Debug.Log("no valid tile found");
         return null;
     }
@@ -94,6 +132,7 @@ public class HexMapGenerator : MonoBehaviour
 
     public Map GenerateMap(int width, int height, MapSettings settings, Vector2 sampleCentre)
     {
+        //Initializing map values
         float[,] values = Noise.GenerateNoiseMap(width, height, settings.noiseSettings, sampleCentre);
 
         AnimationCurve heightCurve_threadsafe = new AnimationCurve(settings.heightCurve.keys);
@@ -101,12 +140,14 @@ public class HexMapGenerator : MonoBehaviour
         float minValue = float.MaxValue;
         float maxValue = float.MinValue;
 
+        //Generate mapvalue from noise map
         for (int i = 0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
             {
                 values[i, j] *= heightCurve_threadsafe.Evaluate(values[i, j]);
 
+                //Update max/min value
                 if (values[i, j] > maxValue)
                 {
                     maxValue = values[i, j];
@@ -122,7 +163,42 @@ public class HexMapGenerator : MonoBehaviour
         return new Map(values, minValue, maxValue);
     }
 
-    public float[,] GenerateFalloffMap(int size)
+    public Map GenerateMapWithMulitplier(int width, int height, MapSettings settings, Vector2 sampleCentre, Map otherMap, bool inverse=false)
+    {
+        //Initializing map values
+        float[,] values = Noise.GenerateNoiseMap(width, height, settings.noiseSettings, sampleCentre);
+
+        AnimationCurve heightCurve_threadsafe = new AnimationCurve(settings.heightCurve.keys);
+
+        float minValue = float.MaxValue;
+        float maxValue = float.MinValue;
+
+        //Generate mapvalue from noise map
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                //Get value at postion
+                values[i, j] *= heightCurve_threadsafe.Evaluate(values[i, j]);
+                values[i, j] *= (inverse) ? Mathf.Lerp(1f, 0f, otherMap.values[i,j]) : otherMap.values[i, j];
+
+                //Update max/min value
+                if (values[i, j] > maxValue)
+                {
+                    maxValue = values[i, j];
+                }
+
+                if (values[i, j] < minValue)
+                {
+                    minValue = values[i, j];
+                }
+            }
+        }
+
+        return new Map(values, minValue, maxValue);
+    }
+
+    public Map GenerateFalloffMap(int size)
     {
         float[,] map = new float[size, size];
 
@@ -138,24 +214,26 @@ public class HexMapGenerator : MonoBehaviour
             }
         }
 
-        return map;
+        return new Map(map, 0f, 1f);
     }
 
+    // Sigmoid curve evaluation for falloff maps
     static float Evaluate(float value)
     {
-        float a = 2f;
-        float b = 1f;
+        const float a = 2f;
+        const float b = 5f;
 
         float returnValue = Mathf.Pow(value, a) / (Mathf.Pow(value, a) + Mathf.Pow(b - b * value, a));
         return Mathf.Lerp(1f, 0f, returnValue);
     }
 }
 
-public struct Map
+//Map class for storing world data
+public class Map
 {
-    public readonly float[,] values;
-    public readonly float minValue;
-    public readonly float maxValue;
+    public float maxValue;
+    public float minValue;
+    public float[,] values;
 
     public Map(float[,] values, float minValue, float maxValue)
     {
